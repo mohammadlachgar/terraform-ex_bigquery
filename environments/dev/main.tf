8760d970b24aaa94400352cb7f3939edf95425aa
Mohammad <mohammad4cj@gmail.com>
2021-01-27 18:55:33 +0000

    change name dataset

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,7 +6,7 @@ module "crTables" {
   source  = "../../modules/crTables/"
   project = var.project
   env     = local.env
-  bq_staging_dataset = var.bq_staging_dataset
+  bq_staging_dataset = "${local.env}_${var.bq_staging_dataset}"
   bq_description = var.bq_description
   bq_location = var.bq_location
   bq_datasets_delete_data_on_destroy = var.bq_datasets_delete_data_on_destroy
